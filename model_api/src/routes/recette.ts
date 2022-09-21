2180aca7844d1cfb3fa3ef52596534f41742b943
Buquet Dorian <dorian.buquet@viacesi.fr>
2022-09-21 14:08:26 +0200

    :sparkles: chiffrement model_api

--- model_api/src/routes/recette.ts
+++ model_api/src/routes/recette.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express-serve-static-core";
 import { RecetteModel } from "~~/Model/model";
+import * as CryptoJS from 'crypto-js'; 
+import { array } from "joi";
 
 require("dotenv").config();
 
 const jwt = require("jsonwebtoken");
 const router = require("express").Router();
+const password = "Password";
 
 router.post("/createRecette", async (req: Request, res: Response) => {
   const isExistingRecette = await RecetteModel.findOne({ name: req.body.name });
@@ -15,6 +18,17 @@ router.post("/createRecette", async (req: Request, res: Response) => {
   }
 
   const RecetteCreate = new RecetteModel(req.body);
+  RecetteCreate.puht = CryptoJS.AES.encrypt(RecetteCreate.puht, password).toString(); 
+  RecetteCreate.gamme = CryptoJS.AES.encrypt(RecetteCreate.gamme, password).toString();
+  const array = new Array;
+  RecetteCreate.ingredients.forEach(function (value) {
+    value = CryptoJS.AES.encrypt(value, password).toString();
+    array.push(value);
+  })
+  RecetteCreate.ingredients = array;
+  if(req.body.description){
+    RecetteCreate.description = CryptoJS.AES.encrypt(req.body.description, password).toString();
+  }
   RecetteCreate.save()
     .then(() => {
       res.status(201).json({ message: "Model created !" });
@@ -44,6 +58,17 @@ router.put("/modify/:name", async (req: Request, res: Response) => {
   }
   try{
     model.set(req.body.modelInfos);
+    model.puht = CryptoJS.AES.encrypt(model.puht, password).toString(); 
+    model.gamme = CryptoJS.AES.encrypt(model.gamme, password).toString();
+    const array = new Array;
+    model.ingredients.forEach(function (value) {
+      value = CryptoJS.AES.encrypt(value, password).toString();
+      array.push(value);
+    })
+    model.ingredients = array;
+    if(req.body.modelInfos.description){
+      model.description = CryptoJS.AES.encrypt(req.body.description, password).toString();
+    }
     model.save().then(() => res.status(200).json({ message: "Model edited!" }));
   }  
   catch(error: any) {
